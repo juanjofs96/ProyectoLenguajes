eb19692d69f2dd993ac078e07c6c983288fdefd7
Eduardo <leortiz@espol.edu.ec>
2018-07-05 10:40:36 -0500

    Creacion lexer y yacc

---
+++ mlexer.py
@@ -0,0 +1,77 @@
+import ply.lex as lex
+import ply.yacc
+
+tokens=['IDENTIDAD','MAS','MENOS','DIVIDE','POR','MODULO','POTENCIA','Y','OINCL','OEXCL','NEGADO','IGUAL','IDENTICO','DIFERENTE','MAYOR',
+        'IF', 'ELSE', 'ELSEIF', 'WHILE', 'PARENTH_IZQ', 'PARENTH_DER', 'DO', 'FOR', 'FOREACH', 'BREAK',
+        'CONTINUE', 'SWITCH', 'CASE', 'DECLARE', 'RETURN', 'REQUIRE', 'INCLUDE', 'GOTO','VAR','OR','AND']
+t_ignore = ' \t'
+t_MAS=r'\+'
+t_MENOS=r'-'
+t_POR=r'\*'
+t_DIVIDE=r'/'
+t_MODULO=r'%'
+t_POTENCIA=r'\*\*'
+t_Y=r'&'
+t_OINCL=r'\|'
+t_OEXCL=r'\^'
+t_NEGADO=r'/~'
+t_IGUAL=r'='
+t_IDENTICO=r'=='
+t_DIFERENTE=r'!='
+
+t_MAYOR=r'>'
+t_MENOR=r'<'
+
+t_IF = r'if'
+t_ELSEIF = r'else\sif'
+t_ELSE = r'else'
+t_WHILE = r'\while'
+t_PARENTH_IZQ = r'\('
+t_PARENTH_DER = r'\)'
+t_DO = r'do'
+t_FOR = r'for'
+t_FOREACH = r'foreach'
+t_BREAK = r'break'
+t_CONTINUE = r'continue'
+t_SWITCH = r'switch'
+t_CASE = r'case'
+t_DECLARE = r'declare'
+t_RETURN = r'return'
+t_INCLUDE = r'include'
+t_GOTO = r'goto'
+t_OR = r'or'
+t_AND = r'and'
+
+
+reserved = {
+    'if': 'IF',
+    'then':'THEN',
+    'while':'WHILE',
+    'else if': 'ELSEIF',
+    'include' : 'INCLUDE',
+    'return': 'RETURN',
+    'case': 'CASE',
+    'switch': 'SWITCH'
+}
+
+
+def t_VAR(p):
+    r'\$[^0-9]+[A-Za-z_0-9]+'
+    p.type = reserved.get(p.value, 'VAR')
+    return p
+
+
+def t_error(t):
+    print(t)
+
+lex.lex()
+
+lex.input("if+*else ifelse $_animal")
+while True:
+    tok = lex.token()
+    if not tok:
+        t_error(tok)
+        break
+    print(tok.value)
+
+
---
+++ yacc.py
@@ -0,0 +1,146 @@
+import ply.yacc as yacc
+import mlexer
+
+vars = mlexer.tokens
+
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE'),
+    ('right', 'UMINUS'),            # Unary minus operator
+)
+
+
+def p_expresion(p):
+    '''expresion: expresion MAS termino | expresion MENOS termino | termino'''
+
+
+def p_termino(p):
+    '''termino: termino POR factor | termino DIVIDE factor | factor'''
+
+
+def p_factor(p):
+    '''factor: NUMERO | VAR | PARENTH_IZQ expresion PARENTH_DER'''
+
+
+def p_asignar(p):
+    '''asignar: VAR IGUAL expresion'''
+    vars[p[1]] = p[3]
+
+
+def p_expresion_suma(p):
+    '''expresion: expresion MAS termino'''
+    p[0] = p[1] + p[3]
+
+
+def p_expresion_resta(p):
+    '''expresion: expresion MENOS termino'''
+    p[0] = p[1] - p[3]
+
+
+def p_termino_mult(p):
+    '''termino: termino POR factor'''
+    p[0] = p[1] * p[3]
+
+
+def p_termino_div(p):
+    '''termino: termino DIVIDE  factor'''
+    p[0] = p[1] / p[2]
+
+
+def p_expresion_termino(p):
+    '''expresion : termino'''
+    p[0] = p[1]
+
+
+def p_termino_factor(p):
+    '''termino: factor'''
+    p[0] = p[1]
+
+
+def p_factor_num(p):
+    '''factor: NUMERO'''
+    p[0] = p[1]
+
+
+def p_expresion_uminus(p):
+    'expresion : MENOS expresion %prec UMINUS'
+    p[0] = -p[2]
+
+
+def p_factor_variable(p):
+    '''factor : VAR'''
+    if vars.has_key(p[1]):
+        p[0] = vars[p[1]]
+    else:
+        print("Undefined Variable", p[1], "in line no.", p.lineno(1))
+
+
+
+def p_factor_expr(p):
+    '''factor: PARENTH_IZQ expresion PARENTH_DER'''
+    p[0] = p[2]
+
+
+def p_logica(p):
+    '''logica: logica OR expresion | logica AND expresion | logica XOR expresion | NEGADO expresion'''
+
+
+def p_logica_or(p):
+    '''logica: logica OR expresion'''
+    p[0] = p[1] or p[3]
+
+
+def p_logica_and(p):
+    '''logica: logica AND expresion'''
+    p[0] = p[1] and p[3]
+
+
+def p_logica_xor(p):
+    '''logica: logica DIFERENTE expresion''' #XOR es equivalente a evaluar si dos expresiones son diferentes o no.
+    p[0] = not(p[1] == p[3])
+
+
+def p_logica_negacion(p):
+    '''logica: NEGADO VAR'''
+    p[0] = not p[1]
+
+
+def p_comparacion(p):
+    '''comparacion : expresion IDENTICO expresion
+                          | expresion DIFERENTE expresion
+                          | expresion MAYOR expresion
+                          | expresion MENOR expresion'''
+    if p[2] == '==':
+        p[0] = p[1] == p[3]
+    elif p[2] == '!=':
+        p[0] = p[1] != p[3]
+    elif p[2] == '>':
+        p[0] = p[1] > p[3]
+    elif p[2] == '<':
+        p[0] = p[1] < p[3]
+
+
+def p_declaracion_if(p):
+    '''declaracion : IF PARENTH_IZQ comparacion declaracion PARENTH_DER '''
+    if p[3]:
+        p[0] = p[5]
+
+
+def p_declaracion_if_else(p):
+    '''declaracion : IF PARENTH_IZQ comparacion declaracion PARENTH_DER
+                    | IF PARENTH_IZQ comparacion declaracion PARENTH_DER, declaracion ELSE '''
+    if p[3]:
+        p[0] = p[5]
+    else:
+        if p[7] is not None:
+            p[0] = p[7]
+
+
+def p_declaracion_while(p):
+    'declaracion : WHILE PARENTH_IZQ comparacion declaracion PARENTH_DER'
+    while(p[3]):
+        p[5];
+
+
+yacc.yacc()
+
